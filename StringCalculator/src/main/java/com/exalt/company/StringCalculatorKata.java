9cda401d769e8ea5eda8952028a1aaba1ea31492
f1charri <florian.charrieau@mbda-systems.com>
2022-08-21 20:02:52 +0200

    [step2] quick code implementation (nothing much to change)

--- StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
+++ StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -5,23 +5,19 @@ public class StringCalculatorKata {
     static final String DELIMITER = ",";
 
     int add(String numbers) {
+        int result = 0;
         if (numbers.isEmpty()) {
             System.out.println("String is empty, returning 0");
-            return 0;
-        }
-        String[] numbersArray = numbers.split(DELIMITER);
-        if (numbersArray.length > 2) {
-            throw new IllegalArgumentException("The argument must take up to two numbers, separated by commas");
-        } else {
-            int result = 0;
-            for (String s : numbersArray) {
-                try {
-                    result += Integer.parseInt(s);
-                } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("Something else than a number was entered : " + s);
-                }
-            }
             return result;
         }
+        String[] numbersArray = numbers.split(DELIMITER);
+        for (String s : numbersArray) {
+            try {
+                result += Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Something else than a number was entered : " + s);
+            }
+        }
+        return result;
     }
 }
