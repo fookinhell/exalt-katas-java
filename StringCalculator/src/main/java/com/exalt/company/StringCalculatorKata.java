23ff7d6c74d2a0e42cfc77aface6d160d3934aff
f1charri <florian.charrieau@mbda-systems.com>
2022-08-21 21:17:23 +0200

    [step5] code implementation

--- StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
+++ StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -1,5 +1,7 @@
 package com.exalt.company;
 
+import java.util.ArrayList;
+
 public class StringCalculatorKata {
 
     static final String COMMA_DELIMITER = ",";
@@ -13,22 +15,34 @@ public class StringCalculatorKata {
             return result;
         }
 
+        ArrayList<Integer> negativeValues = new ArrayList<>(); // init array for negatives
+
         String[] lineArray = numbers.split(NEW_LINE_DELIMITER); // get all the lines in the arg
         String delimiter = COMMA_DELIMITER; // default delimiter is the comma
         for (String line : lineArray) {
-            if(line.startsWith(CUSTOM_DELIMITER_MARKER)) {
+            if (line.startsWith(CUSTOM_DELIMITER_MARKER)) {
                 delimiter = line.replace(CUSTOM_DELIMITER_MARKER, ""); // get the new delimiter
                 continue; // jump to next line
             }
             String[] numberArray = line.split(delimiter);
             for (String number : numberArray) {
                 try {
-                    result += Integer.parseInt(number);
+                    int parsedNumber = Integer.parseInt(number);
+                    if (parsedNumber < 0) { // if parsed number is a negative...
+                        negativeValues.add(parsedNumber); // ... add it to array
+                    } else {
+                        result += parsedNumber; // else add it to the result
+                    }
                 } catch (NumberFormatException e) {
                     throw new IllegalArgumentException("Something else than a number was entered : " + number);
                 }
             }
         }
+
+        if (!negativeValues.isEmpty()) {
+            throw new IllegalArgumentException("Negative values are not allowed. Values entered :" + negativeValues);
+        }
+
         return result;
     }
 }
