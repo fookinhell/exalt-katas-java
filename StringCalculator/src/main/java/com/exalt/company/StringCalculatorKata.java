74810b722a34fb14c501c8a8e504b56462f7108b
f1charri <florian.charrieau@mbda-systems.com>
2022-08-21 20:28:06 +0200

    [step3] code implementation

--- StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
+++ StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -2,7 +2,8 @@ package com.exalt.company;
 
 public class StringCalculatorKata {
 
-    static final String DELIMITER = ",";
+    static final String COMMA_DELIMITER = ",";
+    static final String NEW_LINE_DELIMITER = "\n";
 
     int add(String numbers) {
         int result = 0;
@@ -10,12 +11,15 @@ public class StringCalculatorKata {
             System.out.println("String is empty, returning 0");
             return result;
         }
-        String[] numbersArray = numbers.split(DELIMITER);
-        for (String s : numbersArray) {
-            try {
-                result += Integer.parseInt(s);
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Something else than a number was entered : " + s);
+        String[] lineArray = numbers.split(NEW_LINE_DELIMITER);
+        for (String line : lineArray) {
+            String[] numberArray = line.split(COMMA_DELIMITER);
+            for (String number : numberArray) {
+                try {
+                    result += Integer.parseInt(number);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("Something else than a number was entered : " + number);
+                }
             }
         }
         return result;
