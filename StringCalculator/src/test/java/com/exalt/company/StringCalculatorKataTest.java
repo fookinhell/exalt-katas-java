f63691fe37c759a683ce322277be0c36c636d9d2
f1charri <florian.charrieau@mbda-systems.com>
2022-08-21 19:26:47 +0200

    [step 1] learning JUnit annotations, so implementing it

--- StringCalculator/src/test/java/com/exalt/company/StringCalculatorKataTest.java
+++ StringCalculator/src/test/java/com/exalt/company/StringCalculatorKataTest.java
@@ -1,65 +1,50 @@
 package com.exalt.company;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorKataTest {
 
-    @Test
-    void testWithEmptyString() {
-        // ARRANGE
-        String numbers = "";
-        StringCalculatorKata calculator = new StringCalculatorKata();
+    private StringCalculatorKata calculator;
 
-        // ACT
-        int sum = calculator.add(numbers);
+    @BeforeEach
+    public void initCalculator() {
+        calculator = new StringCalculatorKata();
+    }
 
-        // ASSERT
-        assertEquals(sum, 0);
+    @AfterEach
+    public void undefCalculator() {
+        calculator = null;
     }
 
     @Test
-    void testWithOneNumber() {
-        // ARRANGE
-        String numbers = "18";
-        StringCalculatorKata calculator = new StringCalculatorKata();
-
-        // ACT
-        int sum = calculator.add(numbers);
-
-        // ASSERT
-        assertEquals(sum, 18);
-    }
-
-    @Test
-    void testWithTwoNumbers() {
-        // ARRANGE
-        String numbers = "18,7";
-        StringCalculatorKata calculator = new StringCalculatorKata();
-
-        // ACT
-        int sum = calculator.add(numbers);
-
-        // ASSERT
-        assertEquals(sum, 25);
-    }
-
-    @Test
-    void testWithMoreThanTwoNumbers() {
+    public void testWithMoreThanTwoNumbers() {
         String numbers = "18,7,20";
-        StringCalculatorKata calculator = new StringCalculatorKata();
-
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> calculator.add(numbers));
         assertTrue(exception.getMessage().contains("The argument must take up to two numbers"));
     }
 
     @Test
-    void testWithInvalidCharacter() {
+    public void testWithInvalidCharacter() {
         String numbers = "18;9";
-        StringCalculatorKata calculator = new StringCalculatorKata();
-
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> calculator.add(numbers));
         assertTrue(exception.getMessage().contains("Something else than a number was entered"));
+    }
+
+    @ParameterizedTest(name = "\"{0}\" should return {1}")
+    @CsvSource({
+            "'', 0",
+            "'18', 18",
+            "'18,7', 25",
+            "'54,45', 99"
+    })
+    public void step1_testWithTwoOrLessNumbers(String numbers, int expectedResult) {
+        assertEquals(expectedResult, calculator.add(numbers));
     }
 }
