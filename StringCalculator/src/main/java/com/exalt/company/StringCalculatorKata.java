f9be55383fe756d18ac952917405b71ab73807e7
f1charri <florian.charrieau@mbda-systems.com>
2022-08-21 20:58:56 +0200

    [step4] code implementation

--- StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
+++ StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -4,6 +4,7 @@ public class StringCalculatorKata {
 
     static final String COMMA_DELIMITER = ",";
     static final String NEW_LINE_DELIMITER = "\n";
+    static final String CUSTOM_DELIMITER_MARKER = "//";
 
     int add(String numbers) {
         int result = 0;
@@ -11,9 +12,15 @@ public class StringCalculatorKata {
             System.out.println("String is empty, returning 0");
             return result;
         }
-        String[] lineArray = numbers.split(NEW_LINE_DELIMITER);
+
+        String[] lineArray = numbers.split(NEW_LINE_DELIMITER); // get all the lines in the arg
+        String delimiter = COMMA_DELIMITER; // default delimiter is the comma
         for (String line : lineArray) {
-            String[] numberArray = line.split(COMMA_DELIMITER);
+            if(line.startsWith(CUSTOM_DELIMITER_MARKER)) {
+                delimiter = line.replace(CUSTOM_DELIMITER_MARKER, ""); // get the new delimiter
+                continue; // jump to next line
+            }
+            String[] numberArray = line.split(delimiter);
             for (String number : numberArray) {
                 try {
                     result += Integer.parseInt(number);
